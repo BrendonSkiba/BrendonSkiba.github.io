ff787c55ccb696b9e01be3d396d47378e5c5db73
BrendonSkiba <brendonskiba@gmail.com>
2016-06-29 08:50:35 -0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="first name"><br>
+E-mail: <input type="text" name="last name"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
